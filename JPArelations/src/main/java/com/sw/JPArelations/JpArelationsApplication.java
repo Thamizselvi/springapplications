aae9b6541a0bfa2a952e0fd997aab9d2fed9d21d
Thamizhselvi <thamizh@scrunchworks.com>
2021-12-08 10:45:43 +0530

    new project for all relations in table

---
+++ JPArelations/src/main/java/com/sw/JPArelations/JpArelationsApplication.java
@@ -0,0 +1,13 @@
+package com.sw.JPArelations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpArelationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpArelationsApplication.class, args);
+	}
+
+}
---
+++ JPArelations/src/test/java/com/sw/JPArelations/JpArelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.JPArelations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpArelationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
